fba0345cc8d4735ad60383e149887d5084bc430c
João Vitor <jv.zaniolo@gmail.com>
2024-03-04 19:35:38 -0300

    Add sql.close() to migrate.ts

--- src/db/migrate.ts
+++ src/db/migrate.ts
@@ -1,5 +1,7 @@
 import 'dotenv/config'
 import { migrate } from 'drizzle-orm/better-sqlite3/migrator'
-import { db } from './connection'
+import { db, sql } from './connection'
 
 migrate(db, { migrationsFolder: './drizzle' })
+
+sql.close()
